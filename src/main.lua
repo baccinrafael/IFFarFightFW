33d2ee615dd5dc4ce6259edda81a793cb035f10f
Rafael Baccin <akabacc@gmail.com>
2026-03-20 11:51:41 -0300

    Player State, game starts at fullscreen and player centralized

--- src/main.lua
+++ src/main.lua
@@ -2,34 +2,60 @@ Funcs = require("scripts.funcs")
 Bindings = require("scripts.bindings")
 Player = require("scripts.player")
 
-local const VIRTUAL_W, VIRTUAL_H = 1280, 720
--- Resoluçao da tela que e mostrada no jogo, as artes e fundos deveriam ser de 1920 por 1920 
+local const
+VIRTUAL_W, VIRTUAL_H = 1280, 720
+-- Resoluçao da tela que e mostrada no jogo, as artes e fundos deveriam ser de 1920 por 1920
 
+local realW = love.graphics.getWidth()
+local realH = love.graphics.getHeight()
+
+scaleX = realW / VIRTUAL_W
+scaleY = realH / VIRTUAL_H
+
+local scale = math.min(scaleX, scaleY)
+scaleX = scale
+scaleY = scale
+
+offsetX = (realW - VIRTUAL_W * scale) / 2
+offsetY = (realH - VIRTUAL_H * scale) / 2
 
 function love.load()
 	player = {}
-	player.x = 50
-	player.y = desktopWidth() / 2
+	player.x = VIRTUAL_W / 2
+	player.y = VIRTUAL_H / 2 + VIRTUAL_H / 10
 	player.size = 20
 	player.Nsize = 20
-
+	player.state = 1
+	-- 1 = Standing
+	-- 2 = Crouching
+	-- 3 = Jumping
 end
 
 function love.draw()
+	love.graphics.push()
+	love.graphics.translate(offsetX, offsetY)
+	love.graphics.scale(scaleX, scaleY)
+
+	-- seu jogo aqui
+
 	drawChar(1, player.x, player.y, player.size)
+
+	love.graphics.pop()
 end
 
 function love.update(dt)
 	player.size = player.Nsize
-	if love.keyboard.isDown("a") then
+	if love.keyboard.isDown("a") and player.state == 1 then
 		player.x = player.x - 2
 	end
-	if love.keyboard.isDown("d") then
+	if love.keyboard.isDown("d") and player.state == 1 then
 		player.x = player.x + 2
 	end
 
 	if love.keyboard.isDown("s") then
-		player.size = player.size - 5
+		player.size = player.size - 2
+		player.state = 2
+	else
+		player.state = 1
 	end
-
 end
